d30f215659b3f506f7cb62174b023fb782872f35
Jhonatan Latorre Sierra <jhonatan.latorre@sofka.local>
2025-01-16 10:22:01 -0500

    Implementación de la logica de creación de transacciones para programación reactiva con DTOs:

--- src/main/java/com/training/reactive_bank_system/dto/BalanceResponseDTO.java
+++ src/main/java/com/training/reactive_bank_system/dto/BalanceResponseDTO.java
@@ -1,21 +1,18 @@
 package com.training.reactive_bank_system.dto;
 
-import com.training.reactive_bank_system.model.TransactionType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class TransactionEntryDTO {
+public class BalanceResponseDTO {
 
-    private String accountId;
     private Long userId;
-    private Double amount;
-    private TransactionType transactionType;
+    private String accountId;
+    private Double balance;
 
 }
--- src/main/java/com/training/reactive_bank_system/dto/TransactionResponseDTO.java
+++ src/main/java/com/training/reactive_bank_system/dto/TransactionResponseDTO.java
@@ -1,4 +1,37 @@
 package com.training.reactive_bank_system.dto;
 
+import com.training.reactive_bank_system.model.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TransactionResponseDTO {
+
+    private String transactionId;
+
+    private Long userId;
+
+    private String accountId;
+
+    private Double amount;
+
+    private TransactionType transactionType;
+
+    private Boolean success;
+
+    private Date transactionDate;
+
+    private Double previousBalance;
+
+    private Double currentBalance;
+
+
 }
